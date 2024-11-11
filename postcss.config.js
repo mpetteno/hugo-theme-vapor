c7a79a885d82474115b651b0583577fb26e3aa1a
Matteo Pettenò <matteo.petteno1@gmail.com>
2024-11-11 21:59:58 +0100

    + other content

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,7 @@
 module.exports = {
     plugins: {
         "postcss-import": {
-            "path":  + "./assets/css/"
+            "path": "./assets/css/"
         },
         tailwindcss: {},
         autoprefixer: {},
