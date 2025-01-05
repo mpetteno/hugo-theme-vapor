e2461f2d9b33f0d127abbe304b98f09310ba0244
Matteo Pettenò <matteo.petteno1@gmail.com>
2025-01-05 19:06:19 +0100

    + AoC days

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./layouts/**/*.html', './content/**/*.md'],
+  content: ['./layouts/**/*.html', './content/**/*.md', './assets/js/**/*.js'],
   darkMode: 'selector',
   plugins: [],
   "theme": {
